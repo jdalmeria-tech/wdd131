a6b4f1e35adfad10044d845b9a2bf8a849bdb28a
jdalmeria-tech <alm24008@byui.edu>
2025-01-10 17:09:10 +0800

    done with week01 assignment

---
+++ scripts/typed.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+  strings: ['Web Developer', 'Programmer', 'Freelancer', 'Wizard'],
+  typeSpeed: 60,
+  backSpeed: 60,
+  backDelay: 1200,
+  loop: true
+});
